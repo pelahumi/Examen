59b0a8ac4a64b7f327f4d5e9784cef601b0d259d
Pelayo <phuermij@myuax.com>
2021-12-14 11:15:19 +0100

    Creación de archivos

---
+++ Ejercicio1.py
@@ -0,0 +1,16 @@
+import random
+string = "abcdefghijklmnñopqrstuvwyz"
+string_aleatorio = random.choice(string)
+print(string_aleatorio)
+
+i = int(input("Introduce un número del 1 al 5: "))
+print("La cadena de texto tendrá una longitud de " + str(i))
+
+def Stuart():
+    string = "bcdfghjklmnñpqrstvwxyz"
+    
+    
+    
+        
+
+Stuart()
