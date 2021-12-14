bc5900fb5f919259f5bf82c9a329b3d81f6ed007
Pelayo <phuermij@myuax.com>
2021-12-14 11:18:51 +0100

    prueba

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,14 +1,19 @@
 import random
-string = "abcdefghijklmnñopqrstuvwyz"
-string_aleatorio = random.choice(string)
-print(string_aleatorio)
 
 i = int(input("Introduce un número del 1 al 5: "))
 print("La cadena de texto tendrá una longitud de " + str(i))
 
 def Stuart():
     string = "bcdfghjklmnñpqrstvwxyz"
-    
+    print("Stuart tienes que formar todas las combinaciones posibles con las siguientes letras: ")
+
+    numero = 0
+    while numero < i:
+        string_aleatorio = random.choice(string)
+        print(string_aleatorio)
+        
+
+
     
     
         
