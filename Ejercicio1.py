1a63b57b3903c1363fe19d73a9619f3c81656de0
Pelayo <phuermij@myuax.com>
2021-12-14 12:05:50 +0100

    cambios

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -48,12 +48,67 @@ def Stuart():
                 score_stuart = score_stuart + 5
    
         else:
-            print("La puntucación de Stuart es: " + score_stuart)
+            print("La puntucación de Stuart es: " + str(score_stuart))
             break
 
 
+def Kevin():
+    string = "aeiou"
+    print("Kevin tienes que formar todas las combinaciones posibles con las siguientes letras: ")
+
+    numero = 0
+    while numero < i:
+        string_aleatorio = random.choice(string)
+        print(string_aleatorio,end="")
+        numero = numero + 1
+    
+    score_kevin = 0
+
+    while True:
+        respuesta = input("\n" + "Tienes alguna combinación: ")
+
+        if respuesta == "Sí" or respuesta == "Si" or respuesta == "si" or respuesta == "sí":
+            list(string_aleatorio)
+            lista = list(string_aleatorio)
+            print(lista)
+            combinacion = input("Introduce la combinación: ")
+
+            if combinacion not in lista:
+                print("Error introduce una combinacion con las letras correspondientes.")
+
+            combinaciones = list(combinacion)
+
+            if combinacion in  combinaciones:
+                print("La combinación está repetida")
+
+            #Puntuación
+            if len(combinacion) == 1:
+                score_kevin = score_kevin + 1
+
+            if len(combinacion) == 2:
+                score_kevin = score_kevin + 2
+
+            if len(combinacion) == 3:
+                score_kevin = score_kevin + 3
+            
+            if len(combinacion) == 4:
+                score_kevin = score_kevin + 4
+            
+            if len(combinacion) == 5:
+                score_kevin = score_kevin + 5
+   
+        else:
+            print("La puntucación de Kevin es: " + str(score_kevin))
+            break
 
 
+def ganador():
+    if score_stuart > score_kevin:
+        print("El ganador es Stuart")
+    if score_kevin >score_stuart:
+        print("El ganador es Kevin")
+    else:
+        print("Empate, no hay ganador")
 
 
 
@@ -66,3 +121,5 @@ def Stuart():
         
 
 Stuart()
+Kevin()
+ganador()
