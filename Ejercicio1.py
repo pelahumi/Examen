e5d45797b5d8da2e99ba8fe8063a9667613bff54
Pelayo <phuermij@myuax.com>
2021-12-14 11:55:23 +0100

    mas cambios

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -18,7 +18,9 @@ def Stuart():
         respuesta = input("\n" + "Tienes alguna combinación: ")
 
         if respuesta == "Sí" or respuesta == "Si" or respuesta == "si" or respuesta == "sí":
+            list(string_aleatorio)
             lista = list(string_aleatorio)
+            print(lista)
             combinacion = input("Introduce la combinación: ")
 
             if combinacion not in lista:
@@ -29,8 +31,21 @@ def Stuart():
             if combinacion in  combinaciones:
                 print("La combinación está repetida")
 
-            else:
+            #Puntuación
+            if len(combinacion) == 1:
                 score_stuart = score_stuart + 1
+
+            if len(combinacion) == 2:
+                score_stuart = score_stuart + 2
+
+            if len(combinacion) == 3:
+                score_stuart = score_stuart + 3
+            
+            if len(combinacion) == 4:
+                score_stuart = score_stuart + 4
+            
+            if len(combinacion) == 5:
+                score_stuart = score_stuart + 5
    
         else:
             break
